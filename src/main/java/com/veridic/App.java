d31b0b265a1ca51703625f05a0f4f1828fa5e772
VENKATA <saikadiyala1994@gmail.com>
2017-10-18 10:15:38 -0400

    changes done to ansiblemaster

--- src/main/java/com/veridic/App.java
+++ src/main/java/com/veridic/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello guys!" );
+        System.out.println( "Welcome to veridic solutions" );
     }
 }
